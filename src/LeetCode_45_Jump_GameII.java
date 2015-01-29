3d70434493cef029bc95a370a44cfb4c8e52a761
Amr Rezk <amr.mohamed.rezk@gmail.com>
2015-01-29 19:40:37 +0200

    Jump Game II

---
+++ src/LeetCode_45_Jump_GameII.java
@@ -0,0 +1,25 @@
+public class LeetCode_45_Jump_GameII {
+	public static int jump(int[] A) {
+		if (A.length <= 1)
+			return 0;
+		int maxReach = A[0];
+		int step = A[0];
+		int jump = 1;
+		for (int i = 1; i < A.length; i++) {
+			if (i == A.length - 1)
+				return jump;
+			if (i + A[i] > maxReach)
+				maxReach = i + A[i];
+			step--;
+			if (step == 0) {
+				jump++;
+				step = maxReach - i;
+			}
+		}
+		return jump;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(jump(new int[] { 2, 3, 1, 1, 4 }));
+	}
+}
