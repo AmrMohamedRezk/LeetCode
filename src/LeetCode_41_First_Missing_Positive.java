a206f84fd637f06ef1d5631055333719497ac416
Amr Rezk <amr.mohamed.rezk@gmail.com>
2015-01-30 22:09:38 +0200

    Modify find first missing positive number

--- src/LeetCode_41_First_Missing_Positive.java
+++ src/LeetCode_41_First_Missing_Positive.java
@@ -2,7 +2,7 @@
 public class LeetCode_41_First_Missing_Positive {
 	public static int firstMissingPositive(int[] A) {
 		for(int i=0;i<A.length;){
-			if(A[i]!=i+1&&A[i]>0 && A[i]<=A.length-1 &&A[A[i]-1]!=A[i]){
+			if(A[i]!=i+1&&A[i]>0 && A[i]<A.length &&A[A[i]-1]!=A[i]){
 				int temp = A[A[i]-1];
 				A[A[i]-1]=A[i];
 				A[i]= temp;
