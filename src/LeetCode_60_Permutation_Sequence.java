4f64e6e65a19901a615725fdd8b649171ced24af
Amr Rezk <amr.mohamed.rezk@gmail.com>
2015-01-30 16:10:31 +0200

    Permutation Sequence

--- src/LeetCode_60_Permutation_Sequence.java
+++ src/LeetCode_60_Permutation_Sequence.java
@@ -29,6 +29,7 @@ public class LeetCode_60_Permutation_Sequence {
 		for(int i=2;i<factorials.length;i++)
 			factorials[i]=i*factorials[i-1];
 	}
+	
 	public static void main(String[] args) {
 		for(int i=1;i<=4;i++)
 			System.out.println(getPermutation(2, i));
