ead4815b8aa9a9614b436db5cbcddd314a885f8e
Amr Rezk <amr.mohamed.rezk@gmail.com>
2015-02-02 21:44:33 +0200

    Constant space set matrix to zeros

--- src/LeetCode_73_Set_Matrix_Zeroes.java
+++ src/LeetCode_73_Set_Matrix_Zeroes.java
@@ -1,27 +1,48 @@
-
 public class LeetCode_73_Set_Matrix_Zeroes {
-    public void setZeroes(int[][] matrix) {
-    	if(matrix==null||matrix.length==0)
-    		return;
-        boolean [] rows = new boolean[matrix.length];
-        boolean [] columns = new boolean[matrix[0].length];
-        for(int i=0;i<matrix.length;i++)
-        {
-        	for(int j=0;j<matrix[i].length;j++){
-        		if(matrix[i][j]==0){
-        			rows[i]=true;
-        			columns[j]=true;
-        		}
-        	}
-        }
-        for(int i=0;i<matrix.length;i++)
-        {
-        	for(int j=0;j<matrix[i].length;j++){
-        		if(rows[i]||columns[j]){
-        			matrix[i][j]=0;
-        		}
-        	}
-        }
-    }
+	public static void setZeroes(int[][] matrix) {
+		if (matrix == null || matrix.length == 0)
+			return;
+		boolean row = false;
+		boolean column = false;
+		for (int i = 0; i < matrix.length; i++) {
+			if (matrix[i][0] == 0) {
+				column = true;
+				break;
+			}
+		}
+		for (int i = 0; i < matrix[0].length; i++) {
+			if (matrix[0][i] == 0) {
+				row = true;
+				break;
+			}
+		}
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				if (matrix[i][j] == 0) {
+					matrix[i][0] = 0;
+					matrix[0][j] = 0;
+				}
+			}
+		}
+		for (int i = matrix.length - 1; i >0; i--) {
+			for (int j = matrix[i].length - 1; j > 0; j--) {
+				if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+					matrix[i][j] = 0;
+				}
+			}
+		}
+		if (row) {
+			for (int i = 0; i < matrix[0].length; i++)
+				matrix[0][i] = 0;
+		}
+		if (column) {
+			for (int i = 0; i < matrix.length; i++)
+				matrix[i][0] = 0;
+		}
+	}
 
+	public static void main(String[] args) {
+		setZeroes(new int[][] { new int[] {1,1,1 },
+				new int[] { 0,1,2}});
+	}
 }
