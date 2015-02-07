faa64eb02a51ca471295c7533df5dbd35f50a8de
Amr Rezk <amr.mohamed.rezk@gmail.com>
2015-02-07 16:37:43 +0200

    Binary Tree Maximum Path Sum

---
+++ src/LeetCode_124_Binary_Tree_Maximum_Path_Sum.java
@@ -0,0 +1,44 @@
+public class LeetCode_124_Binary_Tree_Maximum_Path_Sum {
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	int max;
+
+	public int maxPathSum(TreeNode root) {
+		if(root==null)
+			max = 0;
+		else
+			max = root.val;
+		findMax(root);
+		return max;
+	}
+
+	private int findMax(TreeNode node) {
+		if (node == null)
+			return 0;
+
+		int left = Math.max(findMax(node.left), 0);
+		int right = Math.max(findMax(node.right), 0);
+
+		max = Math.max(node.val + left + right, max);
+
+		return node.val + Math.max(left, right);
+	}
+
+	public static void main(String[] args) {
+		TreeNode one = new TreeNode(2);
+		TreeNode two = new TreeNode(-1);
+		TreeNode three = new TreeNode(-2);
+		one.left = two;
+		one.right = three;
+		System.out.println(getMaxPathSum(one));
+
+	}
+}
