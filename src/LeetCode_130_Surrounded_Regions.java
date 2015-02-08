8105c98cd5729b33280416e3058f613daebd9946
Amr Rezk <amr.mohamed.rezk@gmail.com>
2015-02-08 20:55:01 +0200

    Surrounded

--- src/LeetCode_130_Surrounded_Regions.java
+++ src/LeetCode_130_Surrounded_Regions.java
@@ -67,11 +67,4 @@ public class LeetCode_130_Surrounded_Regions {
 		     }  
 		   }  
 		 } 
-	public static void main(String[] args) {
-
-		solve(new char[][] { new char[] { 'X', 'X', 'X', 'X' },
-				new char[] { 'X', 'O', 'O', 'X' },
-				new char[] { 'X', 'X', 'O', 'X' },
-				new char[] { 'X', 'O', 'X', 'X' } });
-	}
 }
