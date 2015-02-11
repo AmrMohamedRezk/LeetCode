7d5c75f2519a9b8d36e909a406028baf1ee05c50
Amr Rezk <amr.mohamed.rezk@gmail.com>
2015-02-11 19:47:17 +0200

    Word Ladder

--- src/LeetCode_127_Word_Ladder.java
+++ src/LeetCode_127_Word_Ladder.java
@@ -1,46 +1,42 @@
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
 
 public class LeetCode_127_Word_Ladder {
 	public static int ladderLength(String start, String end, Set<String> dict) {
 		 if (dict.size() == 0)  
 	            return 0; 
-	 
-	        LinkedList<String> queue = new LinkedList<String>();
-	        LinkedList<Integer> distance = new LinkedList<Integer>();
-	 
-	        queue.add(start);
-	        distance.add(1);
-	 
-	 
-	        while(!queue.isEmpty()){
-	            String current = queue.pop();
-	            int shortest = distance.pop();
-	 
-	            if(current.equals(end)){
-	                return shortest;
-	            }
-	 
-	            for(int i=0; i<current.length(); i++){
-	                char[] currCharArr = current.toCharArray();
-	                for(char c='a'; c<='z'; c++){
-	                    currCharArr[i] = c;
-	 
-	                    String nextWord = new String(currCharArr);
-	                    if(dict.contains(nextWord)){
-	                        queue.add(nextWord);
-	                        distance.add(shortest+1);
-	                        dict.remove(nextWord);
-	                    }
-	                }
-	            }
-	        }
-	 
-	        return 0;
-	}
+		Queue<String> currentLevel = new LinkedList<String>();
+		Queue<String> nextLevel = new LinkedList<String>();
+		Set<String> used = new HashSet<String>();
+		used.add(start);
+		currentLevel.add(start);
+		int level = 1;
+		while (!currentLevel.isEmpty() || !nextLevel.isEmpty()) {
+			while (!currentLevel.isEmpty()) {
+				String current = currentLevel.poll();
+				for (int i = 0; i < current.length(); i++) {
+					for (int j = 'a'; j <= 'z'; j++) {
+						char[] array = current.toCharArray();
+						array[i] = (char) j;
+						String next = new String(array);
+						if (next.equals(end))
+							return level + 1;
+						if (dict.contains(next) && !used.contains(next)) {
+							nextLevel.add(next);
+							used.add(next);
+						}
+					}
+				}
+			}
+			currentLevel = nextLevel;
+			nextLevel = new LinkedList<String>();
+			level++;
 
-	
+		}
+		return 0;
+	}	
 	public static void main(String[] args) {
 		HashSet<String> set = new HashSet<String>();
 		set.add("hot");
