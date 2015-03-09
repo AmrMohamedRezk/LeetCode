38c005132b9eb0a5d329ebc390391b564cb22863
Amr Rezk <amr.mohamed.rezk@gmail.com>
2015-03-09 13:46:33 +0200

    Reverse Bits

---
+++ src/LeetCode_190_Reverse_Bits.java
@@ -0,0 +1,14 @@
+
+public class LeetCode_190_Reverse_Bits {
+    public static int reverseBits(int n) {
+    	int result = 0;
+    	for(int i=0;i<32;i++){
+    		result=result<<1 | (n&1);
+    		n=n>>1;
+    	}
+    	return result;
+    }
+    public static void main(String[] args) {
+		reverseBits(43261596);
+	}
+}
